b1ed8aa404cb14acb73352198039bc95f622b61f
Loopsh2 <vittoria.daelli@gmail.com>
2018-08-10 17:21:38 -0300

    dd

--- app/Tipo.php
+++ app/Tipo.php
@@ -1,12 +1,9 @@
 <?php
 
 namespace App;
-<<<<<<< HEAD
 Use Illuminate\Database\Eloquent\Model;
-=======
 
 use Illuminate\Database\Eloquent\Model;
->>>>>>> 56c177756ad34c0b59cd723f29a655e7e7f842f7
 
 class Tipo extends Model
 {
@@ -16,12 +13,9 @@ class Tipo extends Model
     {
         return $this->hasMany('App\Lugar');
     }
-<<<<<<< HEAD
 
 
 }
 
 ?>
-=======
-}
->>>>>>> 56c177756ad34c0b59cd723f29a655e7e7f842f7
+
