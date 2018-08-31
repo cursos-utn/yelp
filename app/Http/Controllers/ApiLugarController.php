0ca3f5017cf397d565b7608b1066deaa733d79d0
Orlando <orlando.brea@gmail.com>
2018-08-31 16:59:32 -0300

    commit

--- app/Http/Controllers/ApiLugarController.php
+++ app/Http/Controllers/ApiLugarController.php
@@ -15,28 +15,30 @@ class ApiLugarController extends Controller{
         // ?p=<numero de pagina>
         $skip = $request->query('start',0);
         $take = $request->query('length',10);
-        /*
         if ($p<1) {
             $p = 1;
         }
         $skip = ($p-1)*5;
-        */
+
         //echo \App\Lugar::all()->count();
         $list = \App\Lugar::
-        orderBy('reputacion', 'desc')
+        where('nombre', 'like', '%'.$request->query('q', '').'%')
         ->skip($skip)
         ->take(5)
         ->get();
-        return $list;
+        $cantidad = ceil(\App\Lugar::
+        where('nombre', 'like', '%'.$request->query('q', '').'%')->count() / 5);
+        return ['data' => $list, 'paginas' => $cantidad];
 
     }
 
- 
+ /*
         ->take($take)
         ->get();
         $total = \App\Lugar::count();
         $respuesta = ["data" => $list, "recordsTotal" => $total, "recordsFiltered" => $total];
         return $respuesta;
     }
+    */
 
 }
